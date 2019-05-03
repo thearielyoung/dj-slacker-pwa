30e580995741a342628407db59a975714a666ac2
John Reagan <john.reagan@JREAGAN1.local>
2019-05-03 11:21:40 -0500

    Fix songs thanks to Jackie

--- src/Songs/Songs.js
+++ src/Songs/Songs.js
@@ -7,7 +7,7 @@ class Songs extends Component {
     super()
     this.state = {
       'songs': []
-    }; 
+    };
   }
 
   componentWillMount() {
@@ -15,31 +15,44 @@ class Songs extends Component {
   }
 
   componentDidMount() {
-    setInterval(() => {this.getSongs()},60000);
+    setInterval(() => { this.getSongs() }, 60000);
   }
 
   getSongs() {
     fetch("https://dj-slacker.herokuapp.com/nowplaying", {
       mode: 'cors'
     })
-    .then(results => {
-      return results.json();
-    } ) 
-    .then(songs => {
-      this.setState({'songs': songs});
-    })
-    .catch(error => alert("An error happened " + error));
- }
+      .then(results => {
+        return results.json();
+      })
+      .then(songs => {
+        this.setState({ 'songs': songs });
+      })
+      .catch(error => alert("An error happened " + error));
+  }
+  renderSongs = () => {
+    return this.state.songs.map((item, index) => (
+      <Song song={item} />
+    ))
+  }
 
   render() {
     return (
       <div id="test">
         <ul>
-          {this.state.songs.map((item, index) => (
-            <Song song={item} />
-          ))}
+          {this.state.songs && this.state.songs.length > 0
+            ? this.renderSongs() : <p>It's too quiet in here... start some music</p>}
+          {/* { if (this.state.songs && this.state.songs.length > 0) {
+            {
+              this.state.songs.map((item, index) => (
+                <Song song={item} />
+              ))
+            }
+          } else {
+            <p>It's too quiet in here... start some music</p>
+          }} */}
         </ul>
-    </div>
+      </div>
     )
   }
 }
