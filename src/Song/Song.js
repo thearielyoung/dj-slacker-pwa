ad9adb690f17ab7291e49e495f83f413a0916094
John Reagan <john.reagan@cstrauss.backstop.local>
2019-10-24 13:38:57 -0500

    Add basic like/dislike buttons with handlers

--- src/Song/Song.js
+++ src/Song/Song.js
@@ -1,15 +1,68 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import "./Song.css";
+
+const styles = {
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+};
 
 function Song(props) {
+  const { classes, song: songObject } = props;
+  const { user, track } = songObject;
+
+  const handleLike = function(e) {
+ 
+  }
+
+  const handleDislike = function(e) {
+    
+  }
+
   return (
-    <li className="song-info">
-    {props.song.user}: {props.song.track.item.name} -- {props.song.track.item.artists[0].name}
-    <a href="#like">Like</a> || <a href='#dislike'>Dislike</a>
-  </li>
+    <Card>
+      <CardMedia
+        component="iframe"
+        className="album-art"
+        src={`https://open.spotify.com/embed/track/${track.item.id}`}
+        width="100%"
+        height="95"
+        allowtransparency="true"
+        allow="encrypted-media"
+        frameborder="0"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {songObject.user}
+        </Typography>
+        <CardActionArea>
+          <CardActions>
+          <Button variant="outlined" color="primary" className={classes.button} onClick={(e) => handleLike(e)}>
+            Like
+          </Button>
+          <Button variant="outlined" color="secondary" className={classes.button} onClick={(e) => handleDislike(e)}>
+            Dislike
+          </Button>
+          </CardActions>
+        </CardActionArea>
+      </CardContent>
+    </Card>
   );
 }
 
-export default Song;
+export default withStyles(styles)(Song);
 // id
 // spotify_id
 // auth_token
